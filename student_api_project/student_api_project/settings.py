69405c9ac77d8f088b88f3369471ba242f32ee39
TejasBedarkar <tejasaidb11@gmail.com>
2026-03-09 22:33:11 +0530

    inst n config REST

--- student_api_project/student_api_project/settings.py
+++ student_api_project/student_api_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Enable Django REST Framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#All APIs are accessible for now
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
