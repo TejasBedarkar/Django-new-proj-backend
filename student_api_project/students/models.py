6be1735ba1d93fae2c5f96509dbe2faff5bf6d4e
TejasBedarkar <tejasaidb11@gmail.com>
2026-03-09 22:40:51 +0530

    create model and admin

--- student_api_project/students/models.py
+++ student_api_project/students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    course = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):               #This controls how the object appears in Django Admin Panel
+        return self.name
